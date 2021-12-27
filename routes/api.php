61dda768f9581bcf1f9e1c4aed91768e649e6bf9
Leonardo Spuldar <spdleonardo@hotmail.com>
2021-12-27 17:50:23 -0300

    feat: add factory and seeder to Genre

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,10 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::group(['namespace' => 'Api'], function () {
-    Route::resource('categories', 'CategoryController', ['except' => ['create', 'edit']]);
-    Route::resource('genres', 'GenreController', ['except' => ['create', 'edit']]);
+    $exceptCreateAndEdit = [
+        'except' => ['create', 'edit']
+    ];
+
+    Route::resource('categories', 'CategoryController', $exceptCreateAndEdit);
+    Route::resource('genres', 'GenreController', $exceptCreateAndEdit);
 });
